cb1bcb537504e917c657e7968b4e2efbb4c86ab7
bbbingo1 <846790344@qq.com>
2020-02-27 16:49:10 +0800

    调整数组顺序使奇数位于偶数前面

---
+++ jianzhiOFFER/调整数组顺序使奇数位于偶数前面.js
@@ -0,0 +1,17 @@
+/**
+ * 题目描述
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，
+ * 所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+ * @param {array} array 
+ */
+function reOrderArray(array) {
+  // write code here
+  let font = [], behind = [];
+  for (let i of array) {
+    if (i % 2) font.push(i)
+    else {
+      behind.push(i)
+    }
+  }
+  return font.concat(behind)
+}
